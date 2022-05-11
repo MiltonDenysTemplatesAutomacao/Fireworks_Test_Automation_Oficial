2784ad000198d2ec0b116bad19ed05fedfbb6a34
Milton Silva <milton.silva@fire-engine-red.com>
2022-05-11 10:12:37 -0300

    working on step verify action values index 1

--- src/test/java/pages/Records/ActionsPage.java
+++ src/test/java/pages/Records/ActionsPage.java
@@ -16,7 +16,91 @@ public class ActionsPage extends BasePage {
     public static final String ACTION_DATE_FIELD = "#action_date";
     public static final String DETAILS_LABEL = ".//*[@data-field-group-label='Details']";
     public static final String ACTION_SAVE_CHANGES_BUTTON = "actionSaveChangesButton";
+    public static final String ACTION_VISIBILITY_DISABLED_DROPDOWN = "s2id_action_visibility_id";
+    public static final String ACTION_TYPE_DISABLED_DROPDOWN = "s2id_action_type_id";
+    public static final String ACTIONS_MANAGER_TABLE = "#actionsSummaryTable";
+    public static final String ACTIONS_MANAGER_TABLE_ROW1_EDIT_LINK = "actionsSummaryTable_row_0_col_0_link_0";
+    public static final String CREATE_ACTION_PANEL_TITLE = "div#actions span.panel-title.responsive-pull-left";
+    public static final String ACTIONS_MANAGER_SEARCH_FIELD = "actionsSummaryTableControlsTableSearch";
 
+    public static void verifyActionValues(String index){
+        try {
+            if(verifyActionAttributes(index) && verifyActionDetails(index)){
+                ExtentReportsSetUp.testingPass(LogPage.VERIFY_ACTION_VALUES_PASS);
+            }else{
+                FailureDelegatePage.handlePageException(LogPage.VERIFY_ACTION_VALUES_FAIL);
+            }
+        } catch (Exception e) {
+            FailureDelegatePage.handlePageException(LogPage.VERIFY_ACTION_VALUES_FAIL);
+        }
+    }
+    public static boolean verifyActionAttributes(String index)throws Exception{
+        waitElementBy(By.cssSelector(CREATE_ACTION_PANEL_TITLE),20);
+        String staffText = getText(By.id(ACTION_STAFF_DROPDOWN));
+        String actionDateTimeText = getAtribute(By.cssSelector(ACTION_DATE_FIELD),"value");
+        String commentsText = getText(By.id(ACTION_COMMENTS_FIELD));
+        int indexNumber = Integer.parseInt(index);
+
+        if(staffText.contains(mass.get(indexNumber).get("Staff"))
+                && actionDateTimeText.contains(mass.get(indexNumber).get("ActionDateField"))
+                && commentsText.contains(mass.get(indexNumber).get("Comments"))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean verifyActionDetails(String index)throws Exception{
+        waitElementBy(By.cssSelector(CREATE_ACTION_PANEL_TITLE),20);
+        String categoryText = getText(By.id(ACTION_CATEGORY_DROPDOWN));
+        String actionText = getText(By.id(ACTION_DROPDOWN));
+        String actionTypeText = getText(By.id(ACTION_TYPE_DISABLED_DROPDOWN));
+        String actionVisibilityText = getText(By.id(ACTION_VISIBILITY_DISABLED_DROPDOWN));
+        int indexNumber = Integer.parseInt(index);
+
+        if(categoryText.contains(mass.get(indexNumber).get("Category"))
+                && actionText.contains(mass.get(indexNumber).get("Action"))
+                && actionTypeText.contains(mass.get(indexNumber).get("ActionType"))
+                && actionVisibilityText.contains(mass.get(indexNumber).get("ActionVisibility"))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void openAction(String action){
+        try {
+            searchAction(action);
+            waitUntilElementToBeSelected(By.id(ACTIONS_MANAGER_TABLE_ROW1_EDIT_LINK),20);
+            click(By.id(ACTIONS_MANAGER_TABLE_ROW1_EDIT_LINK));
+            ExtentReportsSetUp.testingPass(LogPage.OPEN_ACTION_PASS);
+        } catch (Exception e) {
+            FailureDelegatePage.handlePageException(LogPage.OPEN_ACTION_FAIL);
+        }
+
+    }
+    public static void searchAction(String action)throws Exception{
+        waitUntilElementToBeSelected(By.id(ACTIONS_MANAGER_SEARCH_FIELD),20);
+        write(By.id(ACTIONS_MANAGER_SEARCH_FIELD),action);
+    }
+    public static void verifyActionDataTableValues(String category,String action,String staff,String actionDate,String comments,String index){
+        try {
+            int indexNumber = Integer.parseInt(index);
+            waitElementBy(By.cssSelector(ACTIONS_MANAGER_TABLE),20);
+            String text = getText(By.cssSelector(ACTIONS_MANAGER_TABLE));
+            if (text.contains(mass.get(indexNumber).get(category))
+                    && text.contains(mass.get(indexNumber).get(action))
+                    && text.contains(mass.get(indexNumber).get(staff))
+                    && text.contains(mass.get(indexNumber).get(actionDate))
+                    && text.contains(mass.get(indexNumber).get(comments))
+            ) {
+                ExtentReportsSetUp.testingPass(LogPage.VERIFY_ACTION_DATA_TABLE_VALUES_PASS);
+            } else {
+                FailureDelegatePage.handlePageException(LogPage.VERIFY_ACTION_DATA_TABLE_VALUES_FAIL);
+            }
+        } catch (Exception e) {
+            FailureDelegatePage.handlePageException(LogPage.VERIFY_ACTION_DATA_TABLE_VALUES_FAIL);
+        }
+    }
     public static void updateAction(String person){
         int personNumber = Integer.parseInt(person.toString());
         try {
@@ -46,6 +130,11 @@ public class ActionsPage extends BasePage {
                 waitUntilElementToBeSelected(By.cssSelector(ACTION_DATE_FIELD), 20);
                 BasePage.write(By.cssSelector(ACTION_DATE_FIELD),mass.get(personNumber).get("ActionDateField"));
             }
+            if (mass.get(personNumber).get("Comments") != null) {
+                scrollToElement(By.xpath(DETAILS_LABEL));
+                waitUntilElementToBeSelected(By.id(ACTION_COMMENTS_FIELD), 20);
+                BasePage.write(By.id(ACTION_COMMENTS_FIELD),mass.get(personNumber).get("Comments"));
+            }
             ExtentReportsSetUp.testingPass(LogPage.UPDATE_ACTION_PASS);
         } catch (Exception e) {
             FailureDelegatePage.handlePageException(LogPage.UPDATE_ACTION_FAIL);
@@ -54,7 +143,7 @@ public class ActionsPage extends BasePage {
 
     public static void clickSaveChangesActions(){
         try {
-            waitUntilElementToBeSelected(By.id(ACTION_SAVE_CHANGES_BUTTON),20);
+            wait(2000);
             click(By.id(ACTION_SAVE_CHANGES_BUTTON));
             ExtentReportsSetUp.testingPass(LogPage.SAVE_CHANGES_PASS);
         } catch (Exception e) {
